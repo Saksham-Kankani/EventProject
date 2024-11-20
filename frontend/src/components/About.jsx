9e608693c293dcd36ef8f00a13c03055e2db5423
Saksham-Kankani <superherosaksham@gmail.com>
2024-11-20 19:35:31 +0530

    Second Commit

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -6,17 +6,7 @@ const About = () => {
       <h4>Events</h4>
       <h2>ABOUT</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facilis
-        voluptas provident animi corrupti dolor, ipsum deleniti perferendis
-        veniam neque labore enim odio ratione. Ad commodi rem corporis soluta
-        sed reprehenderit a quasi nihil, odit aperiam exercitationem? Suscipit
-        rerum quibusdam inventore atque veritatis accusantium excepturi,
-        molestias optio perferendis aliquam harum, amet dolore! Animi
-        consequuntur officiis modi eius cumque reiciendis harum ipsum
-        praesentium numquam fugit? Consequatur eveniet recusandae harum hic. Eum
-        illo praesentium optio accusantium, hic quibusdam delectus voluptatem
-        qui a officiis dolorum minima cum illum culpa doloremque quia quidem aut
-        sed laudantium nam repudiandae sequi similique! Esse natus at quas?
+      Welcome to Mark Your Event, your trusted partner for creating unforgettable moments. We specialize in crafting personalized events, from weddings and corporate gatherings to private celebrations and milestone occasions. Our passionate team brings your vision to life with creativity, precision, and attention to detail. Whether it's intimate or grand, we ensure every aspect of your event is seamless and stress-free. With a commitment to excellence and a flair for innovation, we turn your dreams into reality. Let us make your next event extraordinary—because every detail matters, and every moment should be cherished. Your story, our expertise.
       </p>
     </section>
   );
